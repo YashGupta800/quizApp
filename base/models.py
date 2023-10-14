16d5493ed84ceb6bb2dffe4435dde42a17bee3a8
sanskritis <sanskritis@adobe.com>
2023-10-14 15:23:24 +0530

    main models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Topic(models.Model):
+    name=models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Room(models.Model):
+    host = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    topic = models.ForeignKey(Topic,on_delete=models.SET_NULL, null=True)
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+       return self.topic
+    
+
+class Question(models.Model):
+    room = models.ForeignKey(Room,on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:200]
+    
