bf5e10090104042313c0eeaa22305b537142e176
sanskritis <sanskritis@adobe.com>
2023-10-14 15:06:02 +0530

    home page start

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name = "home"),   
+    path('',views.home, name = "home"),  
+    path('maths',views.maths, name="maths"), 
+    path('science',views.science, name="science"),
+    path('english',views.english, name="english"),
 ]
--- base/views.py
+++ base/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 
+
 def home(request):
-    return HttpResponse('Hello Worldß')
+    return render(request,'home.html')
+
+def maths(request):
+    return render(request,'maths.html')
+
+def science(request):
+    return render(request,'science.html')
+
+def english(request):
+    return render(request,'english.html')
